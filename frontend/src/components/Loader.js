b1baf649b5de6fb5d4e9386214df32037de35ed6
laminemessaci <lamine.messaci18@gmail.com>
2022-11-30 15:24:41 +0100

    add loader comp

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+import { PropTypes } from 'prop-types';
+
+/**
+ * Component that displays a Loader
+ *
+ * @param   {string}  type   type of  loader
+ * @param   {string}  color  loader color
+ * @param   {string}  width   Dimensions
+ * @param   {string}  height  Dimensions
+ *
+ * @return {JSX.Element}
+ */
+function Loader({ type, color, width, height }) {
+  return (
+    <div flex items-center justify-center>
+      <ReactLoading type={type} color={color} width={width} height={height} />
+    </div>
+  );
+}
+
+Loader.propTypes = {
+  type: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  width: PropTypes.any || 200,
+  height: PropTypes.any || 200,
+};
+
+export default Loader;
