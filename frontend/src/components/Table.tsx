967315e7266d1d56546f3fa1dbb8458cac154146
laminemessaci <lamine.messaci18@gmail.com>
2022-12-16 00:12:19 +0100

    Cleane

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -1,30 +1,14 @@
+import { Paper } from '@mui/material'
 import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
-import TableCell from '@mui/material/TableCell'
-import TableBody from '@mui/material/TableBody'
+import { ITransaction } from '../redux/userReducerTypes'
 import Row from './../components/Row'
-import { Paper } from '@mui/material'
-import { IAccount, ITransaction } from '../redux/userReducerTypes'
 
-export function createData(date: string, description: string, amount: number, balance: number) {
-  return {
-    date,
-    description,
-    amount,
-    balance,
-  }
-}
 
-export const rows = [
-  createData('June 20th, 2020', 'Golden Sun Backery', 5.0, 2082.79),
-  createData('June 20th, 2020', 'Golden Sun Backery', 10.0, 2087.79),
-  createData('June 20th, 2020', 'Golden Sun Backery', 20.0, 2097.79),
-  createData('June 20th, 2020', 'Golden Sun Backery', 30.0, 2117.79),
-  createData('June 20th, 2020', 'Golden Sun Backery', 40.0, 2147.79),
-  createData('June 20th, 2020', 'Golden Sun Backery', 50.0, 2187.79),
-]
 export const tableHeaders = [{ name: 'date' }, { name: 'description' }, { name: 'amount' }, { name: 'balance' }]
 
 const TransTable: React.FC<ITransaction[]> = (props: ITransaction[]) => {
