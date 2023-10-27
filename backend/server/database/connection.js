8b7c26da49de9cb8a24ac60c56893dc9acf56b5d
laminemessaci <lamine.messaci18@gmail.com>
2023-10-27 18:21:01 +0200

    update env

--- backend/server/database/connection.js
+++ backend/server/database/connection.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://127.0.0.1/argentBankDB';
+const mongoose = require("mongoose");
+const databaseUrl = process.env.DATABASE_URL;
 
 module.exports = async () => {
   try {
     await mongoose.connect(databaseUrl, { useNewUrlParser: true });
-    console.log('Database successfully connected');
+    console.log("Database successfully connected");
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`);
     throw new Error(error);
